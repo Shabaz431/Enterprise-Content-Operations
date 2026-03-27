3e50d158226d6bea4ce78bda846f3850106485d6
Shabaz431 <alikhanshabaz127@gmail.com>
2026-03-27 19:22:12 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from utils.processor import process_content
+
+def main():
+    text = "Hello World"
+    result = process_content(text)
+    print("Output:", result)
+
+if __name__ == "__main__":
+    main()
